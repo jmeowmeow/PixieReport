8bdcae5fa973b488b9e8634c5f61e53caa102d44
jeffrey.miller <jeffrey.miller@gmail.com>
2022-12-29 18:33:44 -0800

    include zuluTime and zuluDayOfMonth

--- pixifier/decoded-metar-parser.js
+++ pixifier/decoded-metar-parser.js
@@ -52,12 +52,17 @@ var stationDesc = function(code, decoded, icaoMap) {
 
 //ob: EGSC 171550Z 23019KT CAVOK 32/14 Q1016
 //example: 171550Z -> 17th of the month, 15:50 UT
-var zuluTime = function(obs) {
-  var regex = /\d{2}(\d{2})(\d{2}Z)/;
-  var result = regex.exec(obs);
-  if (result) { return "" + result[1] + ':' + result[2]; };
-  return "(no time)";
-};
+var withZuluTime = function(params, metar) {
+  var regex = /(\d{2})(\d{2})(\d{2}Z)/;
+  var result = regex.exec(metar);
+  if (result) {
+   params.zuluTime = "" + result[2] + ':' + result[3];
+   params.zuluDayOfMonth = "" + result[1];
+  } else {
+   params.zuluTime = "(no time)";
+  }
+  return params;
+}
 
 var hectoPressure = function(obs) {
   // observation in the form Q1010 = 1010 hectopascals, 1010 hPa
@@ -429,12 +434,6 @@ var stripCarriageReturns = function(maybeItsText) {
   } else {
     return maybeItsText;
   }
-}
-
-// extract to allow accumulating multiple params
-var withZuluTime = function(params, metar) {
-    params.zuluTime = zuluTime(metar);
-    return params;
 }
 
 var decodedToParamObject = function(decodedRawMetarReport) {
