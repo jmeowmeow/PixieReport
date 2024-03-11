e152afd973107189b3c3740a705bcd319680179c
Jeffrey Miller <jeffrey.miller@gmail.com>
2024-03-11 12:04:25 -0700

    /pixie path with temp, location params

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+// parameters: airport code, C/F, which pixie set; optional!
+app.get('/pixie', (req, res) => {
+  const location = req.query.location; // ?location=KSEA (a METAR station)
+  const tempUnit = req.query.temp; // ?temp=30
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(`Hello from ${location}
+The temperature is 30 ${tempUnit}`)
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
