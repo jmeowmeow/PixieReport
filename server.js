950491129c9ab44f6ab140c515c55f8e41f7ccc6
Jeffrey Miller <jeffrey.miller@gmail.com>
2024-03-25 10:39:47 -0700

    express server fetching and parsing METARs

--- server.js
+++ server.js
@@ -2,9 +2,51 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const {compose, Jimp} = require('./compose-async');
+const {decodedToParamObject} = require('./pixifier/decoded-metar-parser');
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  let body = "";
+  let link = "<a href='${url}'>${url}</a><br/>";
+  body += link.replace(/\${url}/g, '/metar?location=KSEA');
+  body += link.replace(/\${url}/g, '/metar?location=KPAE');
+  body += link.replace(/\${url}/g, '/metar?location=KBFI');
+  body += link.replace(/\${url}/g, '/metar?location=KSFO');
+  body += link.replace(/\${url}/g, '/compose');
+  body += link.replace(/\${url}/g, '/pixie');
+  res.send(body);
+});
+
+const fetchMETAR = async (location) => {
+  // note new METAR API endpoint after text server was announced
+  // as discontinued but text URL is working 2024-03-25. Be wary.
+  // https://aviationweather.gov/data/api/#/Data/dataMetars
+  //
+  // also note the bulk all-current-METARs cache updated by minute.
+  // https://aviationweather.gov/data/cache/metars.cache.csv.gz
+  let url = `https://tgftp.nws.noaa.gov/data/observations/metar/decoded/${location}.TXT`;
+  console.log("Fetching", url);
+  let report = await fetch(url).then(response => response.text()).catch(console.error);
+  return report;
+}
+
+app.get('/json', async (req, res) => {
+  const textReport = await fetchMETAR('KSEA');
+  const jsonReport = decodedToParamObject(textReport);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(jsonReport);
+});
+
+app.get('/metar', async (req, res) => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  // if location is not provided, redirect to /metar?location=KSEA
+  const location = req.query.location; // ?location=KSEA (a METAR station)
+  if (location === undefined) {
+    res.redirect('/metar?location=KSEA');
+    return;
+  }
+  let report = await fetchMETAR(location);
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(`Hello from ${location}, report follows:\n ${report}`);
 });
 
 app.get('/compose', async (req, res) => {
