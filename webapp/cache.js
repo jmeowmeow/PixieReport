748669abed52d78b4ff4119bbc3cabd88a5c6d94
Jeffrey Miller <jeffrey.miller@gmail.com>
2024-08-19 09:28:47 -0700

    cache.js tabs to spaces

--- webapp/cache.js
+++ webapp/cache.js
@@ -2,56 +2,56 @@ const cacheDurationMin = 5;
 const cacheDurationMsec = 1000 * 60 * cacheDurationMin;
 
 const put = function (key, value, now) {
-	this.keyValue.set(key, { value: value, ts: now });
-	this.size = this.keyValue.size;
-	return this;
+  this.keyValue.set(key, { value: value, ts: now });
+  this.size = this.keyValue.size;
+  return this;
 }
 
 const get = function (key, tstamp) {
-	let entry = this.keyValue.get(key);
-	if (entry) {
-		let entryTs = entry.ts;
-		if (tstamp - entryTs <= cacheDurationMsec) {
-			return entry.value;
-		}
-	}
+  let entry = this.keyValue.get(key);
+  if (entry) {
+    let entryTs = entry.ts;
+    if (tstamp - entryTs <= cacheDurationMsec) {
+      return entry.value;
+    }
+  }
 
-	return undefined;
+  return undefined;
 }
 
 const del = function (key) {
-	this.keyValue.delete(key);
-	this.size = this.keyValue.size;
-	return this;
+  this.keyValue.delete(key);
+  this.size = this.keyValue.size;
+  return this;
 }
 
 const keys = function () {
-	return this.keyValue.keys();
+  return this.keyValue.keys();
 }
 
 const clear = function() {
-	this.keyValue = new Map();
-	this.size = this.keyValue.size;
-	return this;
+  this.keyValue = new Map();
+  this.size = this.keyValue.size;
+  return this;
 }
 
 const expire = function(tstamp) {
   const dtNow = tstamp;
   let expiredkeys = [...this.keys()].filter(
-	k => (undefined === this.get(k, dtNow))).map(
-		(expiredKey) => this.delete(expiredKey));
+  k => (undefined === this.get(k, dtNow))).map(
+    (expiredKey) => this.delete(expiredKey));
 }
 
 const cache = {
-	put: put,
-	get: get,
-	delete: del,
-	keys: keys,
-	clear: clear,
-	expire: expire,
-	keyValue: undefined,
-	durationMsec: cacheDurationMsec,
-	size: 0
+  put: put,
+  get: get,
+  delete: del,
+  keys: keys,
+  clear: clear,
+  expire: expire,
+  keyValue: undefined,
+  durationMsec: cacheDurationMsec,
+  size: 0
 }
 
 cache.clear();
@@ -66,23 +66,23 @@ const increment = function(client, tstamp) {
 
 const showclients = function() {
         const dtNow = Date.now();
-	this.expire(dtNow);
-	let byCountDescending = [...this.keys()].map( key => [key, this.get(key, dtNow)]).sort( (u, v) => (v[1] - u[1]));
-	return byCountDescending;
+  this.expire(dtNow);
+  let byCountDescending = [...this.keys()].map( key => [key, this.get(key, dtNow)]).sort( (u, v) => (v[1] - u[1]));
+  return byCountDescending;
   };
 
 const clients = {
-	increment: increment,
-	showclients: showclients,
-	put: put,
-	get: get,
-	delete: del,
-	keys: keys,
-	clear: clear,
-	expire: expire,
-	keyValue: undefined,
-	durationMsec: cacheDurationMsec,
-	size: 0
+  increment: increment,
+  showclients: showclients,
+  put: put,
+  get: get,
+  delete: del,
+  keys: keys,
+  clear: clear,
+  expire: expire,
+  keyValue: undefined,
+  durationMsec: cacheDurationMsec,
+  size: 0
 }
 
 clients.clear();
