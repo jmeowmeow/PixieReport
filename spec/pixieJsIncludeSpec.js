000ad8024ae345b16a3bf9d9e819f6715436efcf
Jeffrey Miller <jeffrey.miller@gmail.com>
2024-09-25 23:21:34 -0700

    K3L4 Grand Chenier/Pecan Island geodata

--- spec/pixieJsIncludeSpec.js
+++ spec/pixieJsIncludeSpec.js
@@ -39,7 +39,7 @@ describe("data resource modules should load OK", function() {
   });
 
   it("location map should have this many entries", function() {
-    expect(entryCount).toBe(8712);
+    expect(entryCount).toBe(8713);
   });
 
   it("location map first entry should be", function() {
