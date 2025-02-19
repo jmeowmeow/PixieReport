886db2324506fa8af27a61beedc5c5ee46c30a7a
Jeffrey Miller <jeffrey.miller@gmail.com>
2025-02-19 14:35:38 -0800

    VAHS Rajkot Airport ICAO record.

--- spec/pixieJsIncludeSpec.js
+++ spec/pixieJsIncludeSpec.js
@@ -39,7 +39,7 @@ describe("data resource modules should load OK", function() {
   });
 
   it("location map should have this many entries", function() {
-    expect(entryCount).toBe(8717);
+    expect(entryCount).toBe(8718);
   });
 
   it("location map first entry should be", function() {
