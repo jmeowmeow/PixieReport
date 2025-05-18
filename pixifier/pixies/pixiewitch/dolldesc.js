84a274511d0fd31b548d8716ac78655cbd5260d1
Jeffrey Miller <jeffrey.miller@gmail.com>
2025-05-18 11:20:25 -0700

    witch flying on broom set, Curi Lagann
    From curilagann.art - pixel set of a witch flying on a broom.
    Framed to 125x175. Author metadata added in png properties,
    moved sprite down and right to fit with composition.

---
+++ pixifier/pixies/pixiewitch/dolldesc.js
@@ -0,0 +1,10 @@
+// Curi Lagann's flying witch pixies, commissioned May 2025 from curilagann.art
+const dollsByWeather = {
+0: "a witch with long pale green hair and a pink face flying on a broom, dressed for icy-cold weather with a face cover, scarf, cape, a long-sleeved yellow dress, a tall pale yellow hat with a green hatband, a trailing point and pom-pom, wearing green gloves and boots.",
+1: "a witch with long pale green hair and a smiling pink face flying on a broom, dressed for cold weather with a cape, green boots, a long-sleeved yellow dress, a tall pale yellow hat with a green hatband and a trailing point.",
+2: "a witch with long pale green hair and a smiling pink face flying on a broom, dressed for cool weather with pale green slippers, a short-sleeved yellow dress, a tall pale yellow hat with a green hatband and a trailing point.",
+3: "a witch with long pale green hair and a smiling pink face flying on a broom, dressed for warm weather with green sandals, a yellow sleeveless top, pale olive-colored shorts, a pair of yellow glasses, and a tall pale yellow hat with a green hatband and a trailing point.",
+4: "a witch with long pale green hair and a smiling pink face flying on a broom, dressed for warm weather with bare feet, a yellow sleeveless singlet, a yellow neck band, a pair of yellow glasses, and a tall pale yellow hat with a green hatband and a trailing point."
+};
+
+exports.dollsByWeather = dollsByWeather;
